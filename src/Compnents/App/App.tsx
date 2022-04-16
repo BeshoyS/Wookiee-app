fd122fd5dd1b83171da3229e7e5779729190240f
Beshoy S. Said <72396606+BeshoyS@users.noreply.github.com>
2022-04-16 23:01:28 +0200

    added search feature

--- src/Compnents/App/App.tsx
+++ src/Compnents/App/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Home from "./Compnents/Home/Home";
-import Navbar from "./Compnents/Navbar/Navbar";
+import Home from "../Home/Home";
+import Navbar from "../Navbar/Navbar";
 import { Navigate, Route, Routes } from "react-router-dom";
-import MovieDetails from "./Compnents/MovieDetails/MovieDetails";
-import Search from "./Compnents/Search/Search";
+import MovieDetails from "../MovieDetails/MovieDetails";
+import Search from "../Search/Search";
 
 /*NOTE */
 /*Thank you for this chance, i'm still learning typescript and this is the first time to use it and i wasn't ready enough, i'm not satisfied by my preformance, if you can give me another chance, i will do my best to show my skills again */
--- src/Compnents/Search/SearchInput.tsx
+++ src/Compnents/Search/SearchInput.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router";
-import search from '../../Images/search.png'
-
+import searchImg from "../../Images/search.png";
 // type Props = {}
 
 export default function SearchInput() {
@@ -9,7 +8,7 @@ export default function SearchInput() {
   
   return (
     <div className="search">
-      <img src={search} onClick={()=> navigate('/search')} alt="search-button" />
+      <img src={searchImg} onClick={()=> navigate('/search')} alt="search-button" />
     </div>
   );
 }
